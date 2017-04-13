03b1a21fbe93954618875bb6ff979d52f9b77849
EragonJ <eragonj@eragonj.me>
2017-04-13 16:14:49 +0800

    added 500

---
+++ 500/index.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+var findWords = function(words) {
+    var row1Regex = /[qwertyuiop]+/ig;
+    var row2Regex = /[asdfghjkl]+/ig;
+    var row3Regex = /[zxcvbnm]+/ig;
+    return words.filter((word) => {
+        if (compare(word, row1Regex) || compare(word, row2Regex) || compare(word, row3Regex)) {
+            return true;
+        }
+        return false;
+    });
+};
+
+var compare = function(word, regex) {
+    let match = word.match(regex);
+    if (match && match[0].length === word.length) {
+        return true;
+    } 
+    return false;
+}
+
+module.exports = findWords;
