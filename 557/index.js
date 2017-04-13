59f6b4b2c3467209181f51a180fbc62ff103ed1a
EragonJ <eragonj@eragonj.me>
2017-04-13 15:54:55 +0800

    added 557

---
+++ 557/index.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+    var chunks = s.split(' ');
+    var newChunks = [];
+    chunks.forEach((chunk) => {
+        newChunks.push(chunk.split('').reverse().join(''));
+    });
+    return newChunks.join(' ');
+};
+
+module.exports = reverseWords;
