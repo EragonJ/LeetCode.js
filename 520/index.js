10b6c0efb034866cf718ebaf709088ea1d7eb6c3
EragonJ <eragonj@eragonj.me>
2017-04-17 14:59:21 +0800

    added 520

---
+++ 520/index.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+var detectCapitalUse = function(word) {
+  var patternA = /^[A-Z]+$/;
+  var patternB = /^[a-z]+$/;
+  var patternC = /^[A-Z][a-z]+$/;
+  return !!(word.match(patternB) || word.match(patternA) || word.match(patternC));
+};
+
+module.exports = detectCapitalUse;
