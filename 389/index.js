0755975ffd0fef4504cc5f5a174803dfa8e40440
EragonJ <eragonj@eragonj.me>
2017-04-19 16:31:27 +0800

    added 389

---
+++ 389/index.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+var findTheDifference = function(s, t) {
+  t = t.split('');
+  for (var i = 0; i < s.length; i++) {
+    for (var j = 0; j < t.length; j++) {
+      if (s[i] === t[j]) {
+        t.splice(j, 1);
+        break;
+      }
+    }
+  }
+  return t[0];
+};
+
+module.exports = findTheDifference;
