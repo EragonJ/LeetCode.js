8f4aac56dd3ec81a134b3fdefe2b7859f13f3c89
EragonJ <eragonj@eragonj.me>
2019-01-03 17:52:41 +0800

    added 771

---
+++ 771/index.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} J
+ * @param {string} S
+ * @return {number}
+ */
+var numJewelsInStones = function(J, S) {
+    var count = 0;
+    var jewelsHash = {};
+    var jewels = J.split('');
+    
+    // keep owned jewels into hash, so the search time will be O(1)
+    jewels.forEach(function(jewelType) {
+        jewelsHash[jewelType] = true;
+    });
+    
+    for (var i = 0; i < S.length; i++) {
+        var jewelType = S[i];
+        if (jewelsHash[jewelType]) {
+            count ++;
+        }
+    }
+    
+    return count;
+};
+
+if (module) {
+  module.exports = numJewelsInStones;
+}
