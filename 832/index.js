9033859779d48b629e41c49f6974c6e8f5b2d3b9
EragonJ <eragonj@eragonj.me>
2019-01-12 18:25:02 +0900

    adde 832

---
+++ 832/index.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[][]} A
+ * @return {number[][]}
+ */
+var flipAndInvertImage = function(A) {
+    return A.map(function(eachRow) {
+        return eachRow.reverse().map(function(eachItem) {
+            return (eachItem === 0) ? 1 : 0;
+        });
+    });
+};
+
+if (module.exports) {
+  module.exports = flipAndInvertImage;
+}
