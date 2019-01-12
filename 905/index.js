32beddac294c63703b550f91cf8693adca1e2637
EragonJ <eragonj@eragonj.me>
2019-01-12 19:30:03 +0900

    added 905

---
+++ 905/index.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} A
+ * @return {number[]}
+ */
+var sortArrayByParity = function(A) {
+    var result = [];
+    A.forEach(function(item) {
+        var pos = (item % 2) === 0 ? 0 : result.length;        
+        result.splice(pos, 0, item);
+    });
+    return result;
+};
+
+if (module.exports) {
+  module.exports = sortArrayByParity;
+}
