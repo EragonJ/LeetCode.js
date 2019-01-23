2c8fd22a0e98cb65e46e8cb49c49ccc7861f07e5
EragonJ <eragonj@eragonj.me>
2019-01-23 17:46:42 +0900

    updated 538 with a better solution

--- 538/index.js
+++ 538/index.js
@@ -10,48 +10,23 @@
  * @return {TreeNode}
  */
 var convertBST = function(root) {
-  
-  // [2, 5, 13, ....]
-  var result = [];
-  traverse(root, result);
-  
-  // values are sorted beacuse this is BST
-  var map = {};
-  for (var i = 0; i < result.length; i++) {
-    // map{2 : 2}
-    map[result[i]] = result[i];
-    
-    // map{2 : 20}
-    for (var j = result.length - 1; j > i && result[j] > result[i]; j--) {
-      map[result[i]] += result[j];
+  var sum = 0;
+
+  var changeNode = function(root) {
+    if (!root) {
+      return;
     }
-  }
-  
-  traverse2(root, map);
+    else {
+      changeNode(root.right);
+      sum += root.val;
+      root.val = sum;
+      changeNode(root.left);
+    }
+  };
+
+  changeNode(root);
   return root;
 };
-
-var traverse = function (root, result = []) {
-  if (!root) {
-    return;
-  }
-  else {
-    traverse(root.left, result);
-    result.push(root.val);
-    traverse(root.right, result);
-  }
-};
-
-var traverse2 = function(root, map = {}) {
-  if (!root) {
-    return;
-  }
-  else {
-    traverse2(root.left, map);
-    root.val = map[root.val];
-    traverse2(root.right, map);
-  }
-}
 
 if (module.exports) {
   module.exports = convertBST;
